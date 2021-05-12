5688ec686a4dcd30b226bf679202eed238063e4c
NathanDLR <natandelreal@gmail.com>
2021-05-12 09:15:06 +0200

    Información artículo

--- src/app/pages/modal-articulo/modal-articulo.page.ts
+++ src/app/pages/modal-articulo/modal-articulo.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AlertController, ModalController } from '@ionic/angular';
+import { AlertController, IonInput, ModalController, ToastController } from '@ionic/angular';
 import db from '../../../environments/environment';
 import {User} from 'src/app/shared/user.interface';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -15,14 +15,24 @@ export class ModalArticuloPage implements OnInit {
   user: firebase.default.User
   articuloForm: FormGroup
 
-  constructor(public alertController: AlertController, public modalController: ModalController, private fireAuth: AngularFireAuth)
+  // Textboxes del template
+  @ViewChild('name') nameInput: IonInput;
+  @ViewChild('ingredients') ingredientsInput: IonInput;
+  @ViewChild('alergies') alergiesInput: IonInput;
+  @ViewChild('price') priceInput: IonInput;
+
+
+  constructor(public alertController: AlertController, public modalController: ModalController, private fireAuth: AngularFireAuth, private toastCtrl: ToastController)
   { 
+    // Datos del usuario actual
     fireAuth.user.subscribe((data => {
       this.user = data;
     }));
+
   }
 
   ngOnInit() {
+
     // Formulario en código
     this.articuloForm = new FormGroup({
       name: new FormControl('', [
@@ -45,36 +55,80 @@ export class ModalArticuloPage implements OnInit {
   }
 
   // Validación del formulario
-  async validate(name: String, ingredients: String, alergies: String, price: number){
+  validate(name: String, ingredients: String, alergies: String, price: string): Boolean{  
+    let ok = true;
 
+    if(name == "" || ingredients == "" || alergies == "" || price == "") ok = false
+
+    try{
+      parseInt(price);
+    }
+    catch(error){
+      ok = false
+    }
+    
+    return ok;
   }
 
   // Añadir artículo
   addArticulo(name: string, ingredients: string, alergies: string, price: string){
-    console.log('Nombre:', name, 'Ingredientes:', ingredients, 'Alérgenos:', alergies, 'Precio:', price);
-    
-    // Obtenemos el uid para guardarlo en la colección, así después podremos obtener todos los artículos por el uid
-    let uid = this.user.uid
-    console.log(uid)
-    
-    // Añadimos el artículo a la colección de artículos
-    let collection = 'articulos'
+    // console.log('Nombre:', name, 'Ingredientes:', ingredients, 'Alérgenos:', alergies, 'Precio:', price);
 
-    db.collection(collection).add({
-      nombre: name,
-      ingredientes: ingredients,
-      alergenos: alergies,
-      precio: parseInt(price)
-    })
+    // Validación de los inputs
+    let ok = this.validate(name, ingredients, alergies, price);
 
-    // Limpiamos los inputs
+    if(ok){
 
-    // Mostramos un mensaje para informar al usuario
+      // Obtenemos el uid para guardarlo en la colección, así después podremos obtener todos los artículos por el uid
+      let uid = this.user.uid
+      // console.log(uid)
+      
+      // Añadimos el artículo a la colección de artículos
+      let collection = 'articulos'
 
+      db.collection(collection).add({
+        uid: uid,
+        nombre: name,
+        ingredientes: ingredients,
+        alergenos: alergies,
+        precio: parseInt(price)
+      })
+
+      // Limpiamos los inputs
+      this.clearInputs()
+
+      // Mostramos un mensaje para informar al usuario
+      this.presentToast();  
+    }
+    else{
+      // Pedimos al usuario que revise los inputs
+
+    }
   }
 
   dismissModal(){
     this.modalController.dismiss();
+  }
+
+  // Limpiar los textboxes
+  clearInputs(){
+    this.nameInput.value = "";
+    this.ingredientsInput.value = "";
+    this.alergiesInput.value = "";
+    this.priceInput.value = "";    
+  }
+
+  // Toast message
+  presentToast(){
+    this.toastCtrl.create({
+      animated: true,
+      message: 'Artículo añadido correctamente a la Base de Datos',
+      duration: 2000,
+      translucent: true,
+    }).then((obj) => {
+      obj.present();
+    })
+    
   }
 
 }
