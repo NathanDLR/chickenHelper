19f174b453abd91c53cbce715aa74662b441def9
NathanDLR <natandelreal@gmail.com>
2021-05-12 18:22:26 +0200

    Configurando hosting

--- public/environments/environment.prod.ts
+++ public/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyCh1kV_xFnW1W3dr4tCAfyuysX_aGtUQik",
-    authDomain: "chickenhelper-70bc6.firebaseapp.com",
-    projectId: "chickenhelper-70bc6",
-    storageBucket: "chickenhelper-70bc6.appspot.com",
-    messagingSenderId: "1092527013895",
-    appId: "1:1092527013895:web:a7f28880b8362490e7aaa2",
-    measurementId: "G-29BZHE81EG"
-  }
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCh1kV_xFnW1W3dr4tCAfyuysX_aGtUQik",
+  authDomain: "chickenhelper-70bc6.firebaseapp.com",
+  projectId: "chickenhelper-70bc6",
+  storageBucket: "chickenhelper-70bc6.appspot.com",
+  messagingSenderId: "1092527013895",
+  appId: "1:1092527013895:web:a7f28880b8362490e7aaa2",
+  measurementId: "G-29BZHE81EG"
 };
