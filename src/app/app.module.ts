ed1916e8b6920045bc363ebb5104da7bed8ae4e7
NathanDLR <natandelreal@gmail.com>
2021-06-03 11:48:20 +0200

    Resuelto problema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { firebaseConfig } from 'src/environments/environment';
-import { ModalVentaPipe } from './pages/modal-venta.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ModalVentaPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule, AngularFireModule.initializeApp(firebaseConfig), AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
