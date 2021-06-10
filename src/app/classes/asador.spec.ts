4d01438f5d3fa75fa32e4ac64e368a798fffacee
NathanDLR <natandelreal@gmail.com>
2021-06-10 11:17:51 +0200

    Creada clase asador

---
+++ src/app/classes/asador.spec.ts
@@ -0,0 +1,7 @@
+import { Asador } from './asador';
+
+describe('Asador', () => {
+  it('should create an instance', () => {
+    expect(new Asador()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/asador.ts
@@ -0,0 +1,17 @@
+export class Asador {
+
+    // Atributos & Variables
+    private nombre: string;
+    private direccion: string;
+    // TODO: horario
+
+    // Constructor
+    constructor(nombre: string, direccion: string){
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    
+
+
+}
